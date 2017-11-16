92b52d19bf3d4ec4e48e5919605f96537e4e5824
jcguerrero21 <jotaceguerrerom21@gmail.com>
2017-11-16 01:06:56 +0100

    updated - se añade el borrado multiple y se cambia el loading por defeto

--- src/app/components/lista-libros/lista-libros.component.ts
+++ src/app/components/lista-libros/lista-libros.component.ts
@@ -21,9 +21,10 @@ export class ListaLibrosComponent implements OnInit {
   private allChecked: boolean;
   private borrarListaLibros: Libro[] = new Array();
   modalActions = new EventEmitter<string | MaterializeAction>();
+  modalActions2 = new EventEmitter<string | MaterializeAction>();
 
   constructor(private getListaLibrosService: GetListaLibrosService, private router: Router,
-     private borrarLibroService: BorrarLibroService) { }
+    private borrarLibroService: BorrarLibroService) { }
 
   onSelect(libro: Libro) {
     this.libroSeleccionado = libro;
@@ -38,6 +39,13 @@ export class ListaLibrosComponent implements OnInit {
     this.modalActions.emit({ action: "modal", params: ['close'] });
   }
 
+  openModal2() {
+    this.modalActions2.emit({action:"modal",params:['open']});
+  }
+  closeModal2() {
+    this.modalActions2.emit({action:"modal",params:['close']});
+  }
+
   borrarSelecionado(libro: Libro) {
     this.borrarLibroService.enviarLibro(libro.id).subscribe(
       res => {
@@ -48,10 +56,41 @@ export class ListaLibrosComponent implements OnInit {
       () => {
         this.listaLibros = this.listaLibros.filter(h => h !== libro)
       }
-    )
+    );
   }
 
-  ngOnInit() {
+  actualizarBorrarListaLibros(checked: boolean, libro: Libro) {
+    if (checked) {
+      this.borrarListaLibros.push(libro);
+    } else {
+      this.borrarListaLibros.splice(this.borrarListaLibros.indexOf(libro), 1);
+    }
+  }
+
+  actualizarSeleccionado(checked, boolean) {
+    if (checked) {
+      this.allChecked = true;
+      this.borrarListaLibros = this.listaLibros.slice();
+    } else {
+      this.allChecked = false;
+      this.borrarListaLibros = [];
+    }
+  }
+
+  borrarLibrosSeleccionados() {
+    for (let libro of this.borrarListaLibros) {
+      this.borrarLibroService.enviarLibro(libro.id).subscribe(
+        res => {
+          console.log(res);
+          this.listarLibros();
+          this.closeModal2();
+        },
+        err => console.log(err)
+      );
+    }
+  }
+
+  listarLibros() {
 
     this.getListaLibrosService.getListaLibros().subscribe(
       res => {
@@ -62,6 +101,13 @@ export class ListaLibrosComponent implements OnInit {
         console.log(error);
       }
     );
+
+  }
+
+  ngOnInit() {
+
+    this.listarLibros();
+
   }
 
 }
